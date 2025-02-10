5cbf3e29f285b9febef9f660e05077ccb5d41032
AOjeda06 <andres.ojeda@iesnervion.es>
2025-02-10 11:14:12 +0100

    10/02/2025

--- Tema 4/src/parte1/ej3/Ej3.java
+++ Tema 4/src/parte1/ej3/Ej3.java
@@ -18,45 +18,36 @@ public class Ej3 {
 		}
 
 		// Imprimimos los datos
-		if (r1 != null)
-			System.out.println(r1);
-		if (r2 != null)
-			System.out.println(r2);
+		System.out.println(r1);
+		System.out.println(r2);
 
 		// Modificamos los atributos utilizando los nuevos métodos set
 		try {
-			if (r1 != null) {
-				r1.setX1Y1(2, 2);
-				r1.setX2Y2(4, 4);
-			}
+			r1.setX1Y1(2, 2);
+			r1.setX2Y2(4, 4);
 		} catch (IllegalArgumentException e) {
 			System.out.println("Error al modificar r1: " + e.getMessage());
 		}
 		try {
-			if (r2 != null)
-				r2.setAll(1, 1, 6, 6);
+			r2.setAll(1, 1, 6, 6);
 		} catch (IllegalArgumentException e) {
 			System.out.println("Error al modificar r2: " + e.getMessage());
 		}
 
 		// Imprimimos los datos después de modificarlos
 		System.out.println("\nDespués de las modificaciones:");
-		if (r1 != null)
-			System.out.println(r1);
-		if (r2 != null)
-			System.out.println(r2);
+		System.out.println(r1);
+		System.out.println(r2);
 
 		// Intentamos asignar valores erróneos
 		System.out.println("\nProbando con valores erróneos:");
 		try {
-			if (r1 != null)
-				r1.setX1(-1); // Esto debería lanzar una excepción
+			r1.setX1(-1); // Esto debería lanzar una excepción
 		} catch (IllegalArgumentException e) {
 			System.out.println("Error al asignar valor erróneo a r1: " + e.getMessage());
 		}
 		try {
-			if (r2 != null)
-				r2.setY2(-2); // Esto debería lanzar una excepción
+			r2.setY2(-2); // Esto debería lanzar una excepción
 		} catch (IllegalArgumentException e) {
 			System.out.println("Error al asignar valor erróneo a r2: " + e.getMessage());
 		}
